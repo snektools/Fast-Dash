4a6ba70a6c4b16e6f1918bd433d3bc353065657a
David.Smit <david.smit@gkn.com>
2020-11-25 14:43:49 -0500

    Added drop down data

--- components/controls/drop_down.py
+++ components/controls/drop_down.py
@@ -1,11 +1,34 @@
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+
 try:
     from components.core_component import CoreComponent
 except:
     from Fast_Dash.components.core_component import CoreComponent
 
 class DropDown(CoreComponent):
+    def _update_values(self):
+        self._options = [
+            {
+                'label': key,
+                'value': value,
+            }
+            for key, value in self._values.items()
+        ]
 
-    def _create_values(self):
-        pass
+    def _create_default_value(self):
+        self._value = list(self._values.values())[0]
+
+    def _create_component(self):
+        self.dash_component = dcc.Dropdown(
+            id=self._id,
+            options=self._options,
+            value=self._value,
+        )
+
+    def get_output(self, component_property='options'):
+        return Output(component_id=self._id, component_property=component_property)
+
+    def get_input(self, component_property='value'):
+        return Input(component_id=self._id, component_property=component_property)
