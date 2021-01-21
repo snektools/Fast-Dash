b50a5e1dd0ca72e74e6125d622b78a1ad765d06d
David.Smit <david.smit@gkn.com>
2021-01-21 16:00:26 -0500

    added tabs, not spaces

--- fast_dash/FastDash.py
+++ fast_dash/FastDash.py
@@ -1,28 +1,66 @@
 from dash import Dash
 import dash_html_components as html
-
+import dash_core_components as dcc
 
 class FastDash(Dash):
-    def __init__(self, *args, **kwargs):
+    _default_tab_name = 'default'
+    def __init__(self, has_tabs=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._panes = []
+        self._tabs = {self._default_tab_name :[]}
 
-    def add_pane(self, pane):
-        self._new_pane(pane)
+    def add_pane(self, pane, tab_name=None):
+        self._new_pane(pane, tab_name=tab_name)
 
     def build_pane(self, data_source, visualization, filters=None):
         pass
 
-    def _new_pane(self, pane):
-        self._panes.append(pane)
+    def _new_pane(self, pane, tab_name):
+        tab_name = tab_name or self._default_tab_name
+        if tab_name in self._tabs:
+            self._tabs[tab_name].append(pane)
+        else:
+            self._tabs.update(
+                {
+                    tab_name: [pane]
+                }
+            )
         self._update_layout()
         self._register_callbacks(pane)
 
     def _update_layout(self):
-        self.layout = html.Div(
+        if self._is_multi_tab():
+            self._build_layout_tabbed()
+        else:
+            self._build_layout_single_page()
+
+
+    def _is_multi_tab(self):
+        return len(self._tabs) > 1
+
+    def _build_layout_tabbed(self):
+        self._layout = html.Div(
+            [
+                dcc.Tabs(
+                    [
+                        dcc.Tab(
+                            label=name,
+                            children=self._build_page(tab_name=name)
+                        )
+                        for name, panes in self._tabs.items()
+                    ]
+                )
+            ]
+        )
+
+    def _build_layout_single_page(self):
+        self._layout = self._build_page()
+
+    def _build_page(self, tab_name=None):
+        tab_name = tab_name or self._default_tab_name
+        return html.Div(
             [
                 pane.get_layout()
-                for pane in self._panes
+                for pane in self._tabs[tab_name]
             ]
         )
 
--- fast_dash/data/csv.py
+++ fast_dash/data/csv.py
@@ -1,22 +1,76 @@
 import pandas as pd
 import pathlib
 
-class MultipleCSVDataSource:
+class MultipleFileDataSource:
     def __init__(
             self,
             csv_folder_path: str,
+            meta_data_engine,
+            read_csv_engine = pd.read_csv,
             file_name_pattern: str = '*TestData.csv',
             recursive: bool = False,
-            meta_data_engine=BepcoMeta,
             cache=None,
+            load_filter=None,
     ):
         self._csv_folder_path = pathlib.Path(csv_folder_path)
         self._recursive = recursive
         self._cache = cache
+        self._file_name_pattern = file_name_pattern
+        self._read_csv_engine = read_csv_engine
         self._meta_engine = meta_data_engine()
+        self._files_paths = None
+        self._files = dict()
+        self._load_filter = load_filter
+        self._load_files()
+
+    def _filter_file_paths(self):
+        if self._load_filter:
+            self._files_paths = {
+                key: value
+                for key, value in self._files_paths.items()
+                if self._load_filter(key)
+            }
 
     def _load_files(self):
-        pass
+        if self._recursive:
+            self._get_file_paths_recusrive()
+        else:
+            self._get_file_paths()
+
+        self._filter_file_paths()
+
+        for file_stem, file_path in self._files_paths.items():
+            print(file_stem)
+            temp_file = self._read_csv_engine(file_path)
+            self._files.update(
+                {
+                    file_stem: None
+                }
+            )
+            self._meta_engine.add_cycle(file_stem, temp_file)
+
+    def _get_file_paths(self):
+        self._files_paths = {
+            file_path.stem: file_path
+            for file_path in self._csv_folder_path.glob(self._file_name_pattern)
+        }
+
+    def _get_file_paths_recusrive(self):
+        self._files_paths = {
+            file_path.stem: file_path
+            for file_path in self._csv_folder_path.rglob(self._file_name_pattern)
+        }
+
+    def _get_ids(self, ids):
+        return {
+            id: self._read_csv_engine(self._files[id])
+            for id in ids
+        }
+
+    @property
+    def ids(self):
+        return tuple(self._files.keys())
+
 
     def query(
             self,
@@ -28,8 +82,8 @@ class MultipleCSVDataSource:
             cycle_id=None,
     ):
         if cycle_id:
-            if cycle_id in self._cycles:
-                return self._cycles[cycle_id]
+            if cycle_id in self._files:
+                return self._files[cycle_id]
             else:
                 raise Exception('The cycle requested.')
 
@@ -40,7 +94,6 @@ class MultipleCSVDataSource:
             type,
             machine,
         )
-
         return self._get_ids(queried_cycle_ids)
 
 
