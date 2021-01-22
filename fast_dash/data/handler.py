ceb56747a7bb12d9d7ce43ea5643a2b45948c1cb
David.Smit <david.smit@gkn.com>
2021-01-22 12:56:24 -0500

    updated handler to be able to use a list of functions for post processing

--- fast_dash/data/handler.py
+++ fast_dash/data/handler.py
@@ -22,14 +22,28 @@ class DefaultHandler(Handler):
         self._static_data_arguments = static_data_arguments or dict()
         self._data_source = data_source
         self._queried_data = None
-        self._post_processing_function = post_processing_function
+        self._post_processing_function = None
+        self._create_post_process_function(post_processing_function)
         self._processed_data = None
         self._aggregating_function = aggregating_function
         self._final_data = None
 
+    def _create_post_process_function(self, post_processing_function):
+        if callable(post_processing_function):
+            self._post_processing_function = post_processing_function
+        elif isinstance(post_processing_function, (list, tuple, set)):
+            def post_processing_functions(data):
+                for function in post_processing_function:
+                    data = function(data)
+                return data
+            self._post_processing_function = post_processing_functions
+        else:
+            raise('A function or colleciton of functions needs to be passed for post_processing_function')
+
     def __call__(self, *args, **kwargs):
         kwargs = self._prepare_arguments(**kwargs)
         self._queried_data = self._data_source(**kwargs)
+        self._processed_data = self._queried_data
         self._processed_data = self._post_processing_function(self._queried_data)
         self._final_data = self._aggregating_function(self._processed_data)
 
